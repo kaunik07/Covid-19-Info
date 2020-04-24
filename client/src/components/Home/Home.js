a4d71ef9c098a8f8325e6419a8f31e92ab2d13b1
kaunik07 <kaunikkamila01@gmail.com>
2020-04-24 23:38:12 +0530

    minor fixes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,39 +1,19 @@
 import React from 'react';
 import {Link } from 'react-router-dom';
 import logo from './coronavirus.png';
+// import { Grommet, Box, Button, Grid, Text } from "grommet";
 import './Home.css';
 
-class Home extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      apiResponse: '',
-    }
-  }
-
-  callAPi(){
-    fetch("/testAPI")
-    .then((res) => res.json())
-    .then((res) => {
-      this.setState({
-        apiResponse: res.message
-      });
-    });
-  }
-
-  componentDidMount(){
-    return this.callAPi();
-  }
-  render() {
-    return (
-      <div className="home">
-        <header className="home-header">
-          <img src={logo} className="home-logo" alt="logo" />
-          <Link className='home-link' to="/CoronaTracker">Covid-19 Tracker</Link>
-        </header>
-      </div>
-    );
-  }
+function Home(){
+  return (
+    <div className="home">
+      <header className="home-header">
+       <h1>C<img src={logo} className="home-logo" alt="logo" />VID-19</h1>
+        <Link className='home-link' to="/CoronaTracker">Covid-19 Tracker</Link>
+        <Link className='home-link' to="/News">Covid-19 News</Link>
+      </header>
+    </div>
+  )
 }
 
 export default Home;
